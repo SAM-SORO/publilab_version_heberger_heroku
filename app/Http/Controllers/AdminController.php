a3cf5a9cdadf410b4153730af11174078fe51279
= <=>
2024-12-29 19:40:17 +0000

    rectification5 version 1.2 heberger

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,6 +21,7 @@ class AdminController extends Controller
 
     public function index()
     {
+        @dd("ici");
         $nombreChercheurs = Chercheur::count();
         $nombreVisiteurs = Visiteur::count();
         $nombreArticles = Article::count();
