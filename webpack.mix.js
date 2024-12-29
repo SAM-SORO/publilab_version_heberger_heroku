236fb4cced881a9b7411474ceaa8ba2c69565234
= <=>
2024-12-29 19:17:30 +0000

    rectification3 version 1.2 heberger

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,7 @@ let mix = require('laravel-mix');
 // Compiler app.js
 mix.js('resources/js/app.js', 'public/assets/js')
 
-// Ajouter Select2
+// Ajouter Select2 JS
    .scripts('resources/select2/select2.min.js', 'public/assets/js/select2.min.js')
 
 // Ajouter jQuery
@@ -12,11 +12,20 @@ mix.js('resources/js/app.js', 'public/assets/js')
 // Ajouter Bootstrap JS
    .scripts('resources/bootstrap/js/bootstrap.min.js', 'public/assets/js/bootstrap.min.js')
 
-// Chemin public
-   .setPublicPath('public');
+// Ajouter Bootstrap CSS
+   .css('resources/bootstrap/css/bootstrap.min.css', 'public/assets/css/bootstrap.min.css')
+
+// Ajouter FontAwesome CSS
+   .css('resources/fontawesome-free-6.5.1-web/css/fontawesome.min.css', 'public/assets/css/fontawesome.min.css')
+
+// Ajouter Select2 CSS
+   .css('resources/select2/select2.min.css', 'public/assets/css/select2.min.css')
 
 // Compiler les fichiers CSS aussi
-mix.css('resources/css/app.css', 'public/assets/css');
+   .css('resources/css/app.css', 'public/assets/css')
+
+// Chemin public
+   .setPublicPath('public');
 
 // Ajouter la version pour le cache busting
 mix.version();
