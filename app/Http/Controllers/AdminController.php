d6c1e375e30f01f74a4acbd74b1a5e07d419d398
= <=>
2024-12-29 20:12:46 +0000

    rectification9 version 1.2 heberger

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -23,18 +23,18 @@ class AdminController extends Controller
     {
 
         $nombreChercheurs = Chercheur::count();
-        $nombreVisiteurs = Visiteur::count();
-        $nombreArticles = Article::count();
-        $nombreRevues = Revue::count();
-        $nombreUmris = UMRI::count();
-        $nombreLaboratoires = Laboratoire::count();
-        $nombreAxeRecherche = AxeRecherche::count();
-        $nombreThemes = Theme::count();
-        $nombreDoctorant = Doctorant::count();
-        $nombreGrade = Grade::count();
-        $nombreBdIndexation = BdIndexation::count();
+        // $nombreVisiteurs = Visiteur::count();
+        // $nombreArticles = Article::count();
+        // $nombreRevues = Revue::count();
+        // $nombreUmris = UMRI::count();
+        // $nombreLaboratoires = Laboratoire::count();
+        // $nombreAxeRecherche = AxeRecherche::count();
+        // $nombreThemes = Theme::count();
+        // $nombreDoctorant = Doctorant::count();
+        // $nombreGrade = Grade::count();
+        // $nombreBdIndexation = BdIndexation::count();
 
-        @dd(compact('nombreChercheurs', 'nombreVisiteurs', 'nombreArticles', 'nombreRevues', 'nombreUmris', 'nombreLaboratoires', 'nombreAxeRecherche', 'nombreThemes', 'nombreDoctorant', 'nombreGrade', 'nombreBdIndexation'));
+        @dd(compact('nombreChercheurs'));
 
 
         return view('lab.admin.index', compact('nombreChercheurs', 'nombreVisiteurs', 'nombreArticles', 'nombreRevues', 'nombreUmris', 'nombreLaboratoires', 'nombreAxeRecherche', 'nombreThemes' , 'nombreDoctorant' , 'nombreGrade', 'nombreBdIndexation'));
